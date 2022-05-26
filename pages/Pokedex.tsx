97accf6d8459a2cd80396a412bd18a3a831b09cf
printf-ana <ana_carolina_88@live.com>
2022-05-26 14:09:24 -0300

    feat: makes requistion and shows the name

--- pages/Pokedex.tsx
+++ pages/Pokedex.tsx
@@ -1,44 +1,46 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useToggle } from 'react-use';
 import { Modal } from '../src/componentes';
 import { Card } from '../src/componentes/card';
 
-const pokemonsArray: string[] = [
-  'bulbasaur',
-  'ivysaur',
-  'venusaur',
-  'charmander',
-  'charmeleon'
-];
+interface PokemonList {
+  name: string;
+  url: string;
+}
 
 export const Pokedex: React.FC = () => {
   const [modal, setModal] = useToggle(false);
-  const [pokemons, setPokemons] = useState<string[]>(pokemonsArray);
-  const [selectedPokemon, setSelectedPokemon] = useState<string | undefined>(
-    ''
-  );
+  const [pokemons, setPokemons] = useState<PokemonList[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<
+    PokemonList | undefined
+  >(undefined);
 
-  const setSeeMoreEvent = (pokemon: string) => {
+  const setCardClick = (pokemon: PokemonList) => {
     setModal();
     setSelectedPokemon(pokemon);
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon').then((response) => {
+      setPokemons(response.data.results);
+    });
+  }, []);
 
   return (
     <div>
       <h1>Pokedex</h1>
       <div style={{ display: 'flex' }}>
         {pokemons.map((pokemon) => (
-          <Card key={pokemon} onClick={() => setSeeMoreEvent(pokemon)}>
-            Pokemons: {pokemon}
+          <Card key={pokemon.name} onClick={() => setCardClick(pokemon)}>
+            Pokemons: {pokemon.name}
           </Card>
         ))}
       </div>
 
       {modal && (
         <Modal isOpen={modal} isClose={() => setModal(false)}>
-          <h2> {selectedPokemon}</h2>
+          <h2> {selectedPokemon?.name}</h2>
         </Modal>
       )}
     </div>
--- src/componentes/card/StyledCard.ts
+++ src/componentes/card/StyledCard.ts
@@ -5,4 +5,9 @@ export const StyledCard = styled.div`
   border-radius: 2rem;
   margin: 1rem;
   width: 10rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
