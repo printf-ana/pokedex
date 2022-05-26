227bad541480ab4ac53cc04a1670886cc05ab37b
printf-ana <ana_carolina_88@live.com>
2022-05-26 11:37:33 -0300

    feat: adds react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import Home from '.';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Home />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next';
 import { Card } from '../src/componentes/card';
 
 const Home: NextPage = () => {
-  return <Card>oie</Card>;
+  return (
+    <>
+      <Card>oie</Card>
+    </>
+  );
 };
 
 export default Home;
